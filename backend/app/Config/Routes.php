79589752b93597eeffabe282b52c1d097e68de52
HilmySA <hilmysaputra1.0@gmail.com>
2025-11-09 22:03:20 +0700

    Update

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -10,7 +10,6 @@ $routes->get('/', 'Home::index');
 $routes->group('api', ['namespace' => 'App\Controllers\Api'], function ($routes) {
     $routes->get('karyawan', 'KaryawanController::index');
     $routes->post('karyawan', 'KaryawanController::create');
-
     $routes->get('jam-kantor', 'JamKantorController::index');
     $routes->post('jam-kantor/update', 'JamKantorController::updateBatch');
 });
